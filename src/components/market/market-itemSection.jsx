9ca6f78163ae27dd03fec972caab7752377f97cb
2son4u <87467607+2son4u@users.noreply.github.com>
2023-01-19 16:41:05 +0700

    feat : make item section
    grid broken

---
+++ src/components/market/market-itemSection.jsx
@@ -0,0 +1,250 @@
+const MarketItemSection = () => {
+    const cardBackground = {
+        margin: "1rem 0",
+        boxShadow: "0 0 5px lightgray",
+        padding: "1rem",
+        borderRadius: ".5rem",
+        marginTop: "100px" // This one for create space from Head Massage "Market Item"
+    };
+
+    const itemCard = {
+        boxShadow: "0 0 5px lightgray",
+        borderRadius: ".5rem"
+    };
+
+    return(
+        <div className="container">
+            <div className="dark:bg-adopsoftdark dark:text-adoplight dark:shadow-md" style={cardBackground}>
+                <div>
+                    All 25 Result
+                </div>
+                <hr />
+                <div className="grid grid-cols-5 gap-6 pt-5">
+
+                    <div className="dark:bg-adoplight text-adopdark dark:shadow-md cursor-pointer" style={itemCard}>
+                        <div className="flex relative">
+                            <img className="rounded-md" src="https://chpic.su/_data/stickers/s/Sad_Kyaru_chan/Sad_Kyaru_chan_033.webp" alt="" />
+                        </div>
+                        <div className="p-2">
+                            <div className="flex relative">
+                                <img className="rounded-full h-10" src="https://chpic.su/_data/stickers/s/Sad_Kyaru_chan/Sad_Kyaru_chan_005.webp" alt="" />
+                                <div className="text-sm absolute right-0 bg-adoppix rounded-sm text-adoplight pl-1 pr-1 bottom-14 m-1">
+                                    200 Ac
+                                </div>
+                                <div className="pl-3 truncate">
+                                    <div className="text-base">
+                                        Random cat Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum perferendis tempore voluptate, sit tenetur quam quo soluta! Autem reiciendis facilis, sunt, cumque aliquam tempore provident nemo maiores neque possimus ipsam.
+                                    </div>
+                                    <div className="text-xs ml-3">
+                                        this cat is some Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta molestias cumque quae fugit a. Voluptates, explicabo. Eveniet similique error omnis laudantium consequatur ex, aspernatur consectetur, delectus maxime velit placeat vitae.
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="dark:bg-adoplight text-adopdark dark:shadow-md cursor-pointer" style={itemCard}>
+                        <div className="flex relative">
+                            <img className="rounded-md" src="https://chpic.su/_data/stickers/s/Sad_Kyaru_chan/Sad_Kyaru_chan_033.webp" alt="" />
+                        </div>
+                        <div className="p-2">
+                            <div className="flex relative">
+                                <img className="rounded-full h-10" src="https://chpic.su/_data/stickers/s/Sad_Kyaru_chan/Sad_Kyaru_chan_005.webp" alt="" />
+                                <div className="text-sm absolute right-0 bg-adoppix rounded-sm text-adoplight pl-1 pr-1 bottom-14 m-1">
+                                    200 Ac
+                                </div>
+                                <div className="pl-3 truncate">
+                                    <div className="text-base">
+                                        Random cat Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum perferendis tempore voluptate, sit tenetur quam quo soluta! Autem reiciendis facilis, sunt, cumque aliquam tempore provident nemo maiores neque possimus ipsam.
+                                    </div>
+                                    <div className="text-xs ml-3">
+                                        this cat is some Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta molestias cumque quae fugit a. Voluptates, explicabo. Eveniet similique error omnis laudantium consequatur ex, aspernatur consectetur, delectus maxime velit placeat vitae.
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div className="dark:bg-adoplight text-adopdark dark:shadow-md cursor-pointer" style={itemCard}>
+                        <div className="flex relative">
+                            <img className="rounded-md" src="https://chpic.su/_data/stickers/s/Sad_Kyaru_chan/Sad_Kyaru_chan_033.webp" alt="" />
+                        </div>
+                        <div className="p-2">
+                            <div className="flex relative">
+                                <img className="rounded-full h-10" src="https://chpic.su/_data/stickers/s/Sad_Kyaru_chan/Sad_Kyaru_chan_005.webp" alt="" />
+                                <div className="text-sm absolute right-0 bg-adoppix rounded-sm text-adoplight pl-1 pr-1 bottom-14 m-1">
+                                    200 Ac
+                                </div>
+                                <div className="pl-3 truncate">
+                                    <div className="text-base">
+                                        Random cat Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum perferendis tempore voluptate, sit tenetur quam quo soluta! Autem reiciendis facilis, sunt, cumque aliquam tempore provident nemo maiores neque possimus ipsam.
+                                    </div>
+                                    <div className="text-xs ml-3">
+                                        this cat is some Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta molestias cumque quae fugit a. Voluptates, explicabo. Eveniet similique error omnis laudantium consequatur ex, aspernatur consectetur, delectus maxime velit placeat vitae.
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="dark:bg-adoplight text-adopdark dark:shadow-md cursor-pointer" style={itemCard}>
+                        <div className="flex relative">
+                            <img className="rounded-md" src="https://chpic.su/_data/stickers/s/Sad_Kyaru_chan/Sad_Kyaru_chan_033.webp" alt="" />
+                        </div>
+                        <div className="p-2">
+                            <div className="flex relative">
+                                <img className="rounded-full h-10" src="https://chpic.su/_data/stickers/s/Sad_Kyaru_chan/Sad_Kyaru_chan_005.webp" alt="" />
+                                <div className="text-sm absolute right-0 bg-adoppix rounded-sm text-adoplight pl-1 pr-1 bottom-14 m-1">
+                                    200 Ac
+                                </div>
+                                <div className="pl-3 truncate">
+                                    <div className="text-base">
+                                        Random cat Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum perferendis tempore voluptate, sit tenetur quam quo soluta! Autem reiciendis facilis, sunt, cumque aliquam tempore provident nemo maiores neque possimus ipsam.
+                                    </div>
+                                    <div className="text-xs ml-3">
+                                        this cat is some Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta molestias cumque quae fugit a. Voluptates, explicabo. Eveniet similique error omnis laudantium consequatur ex, aspernatur consectetur, delectus maxime velit placeat vitae.
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div className="dark:bg-adoplight text-adopdark dark:shadow-md cursor-pointer" style={itemCard}>
+                        <div className="flex relative">
+                            <img className="rounded-md" src="https://chpic.su/_data/stickers/s/Sad_Kyaru_chan/Sad_Kyaru_chan_033.webp" alt="" />
+                        </div>
+                        <div className="p-2">
+                            <div className="flex relative">
+                                <img className="rounded-full h-10" src="https://chpic.su/_data/stickers/s/Sad_Kyaru_chan/Sad_Kyaru_chan_005.webp" alt="" />
+                                <div className="text-sm absolute right-0 bg-adoppix rounded-sm text-adoplight pl-1 pr-1 bottom-14 m-1">
+                                    200 Ac
+                                </div>
+                                <div className="pl-3 truncate">
+                                    <div className="text-base">
+                                        Random cat Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum perferendis tempore voluptate, sit tenetur quam quo soluta! Autem reiciendis facilis, sunt, cumque aliquam tempore provident nemo maiores neque possimus ipsam.
+                                    </div>
+                                    <div className="text-xs ml-3">
+                                        this cat is some Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta molestias cumque quae fugit a. Voluptates, explicabo. Eveniet similique error omnis laudantium consequatur ex, aspernatur consectetur, delectus maxime velit placeat vitae.
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="dark:bg-adoplight text-adopdark dark:shadow-md cursor-pointer" style={itemCard}>
+                        <div className="flex relative">
+                            <img className="rounded-md" src="https://chpic.su/_data/stickers/s/Sad_Kyaru_chan/Sad_Kyaru_chan_033.webp" alt="" />
+                        </div>
+                        <div className="p-2">
+                            <div className="flex relative">
+                                <img className="rounded-full h-10" src="https://chpic.su/_data/stickers/s/Sad_Kyaru_chan/Sad_Kyaru_chan_005.webp" alt="" />
+                                <div className="text-sm absolute right-0 bg-adoppix rounded-sm text-adoplight pl-1 pr-1 bottom-14 m-1">
+                                    200 Ac
+                                </div>
+                                <div className="pl-3 truncate">
+                                    <div className="text-base">
+                                        Random cat Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum perferendis tempore voluptate, sit tenetur quam quo soluta! Autem reiciendis facilis, sunt, cumque aliquam tempore provident nemo maiores neque possimus ipsam.
+                                    </div>
+                                    <div className="text-xs ml-3">
+                                        this cat is some Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta molestias cumque quae fugit a. Voluptates, explicabo. Eveniet similique error omnis laudantium consequatur ex, aspernatur consectetur, delectus maxime velit placeat vitae.
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div className="dark:bg-adoplight text-adopdark dark:shadow-md cursor-pointer" style={itemCard}>
+                        <div className="flex relative">
+                            <img className="rounded-md" src="https://chpic.su/_data/stickers/s/Sad_Kyaru_chan/Sad_Kyaru_chan_033.webp" alt="" />
+                        </div>
+                        <div className="p-2">
+                            <div className="flex relative">
+                                <img className="rounded-full h-10" src="https://chpic.su/_data/stickers/s/Sad_Kyaru_chan/Sad_Kyaru_chan_005.webp" alt="" />
+                                <div className="text-sm absolute right-0 bg-adoppix rounded-sm text-adoplight pl-1 pr-1 bottom-14 m-1">
+                                    200 Ac
+                                </div>
+                                <div className="pl-3 truncate">
+                                    <div className="text-base">
+                                        Random cat Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum perferendis tempore voluptate, sit tenetur quam quo soluta! Autem reiciendis facilis, sunt, cumque aliquam tempore provident nemo maiores neque possimus ipsam.
+                                    </div>
+                                    <div className="text-xs ml-3">
+                                        this cat is some Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta molestias cumque quae fugit a. Voluptates, explicabo. Eveniet similique error omnis laudantium consequatur ex, aspernatur consectetur, delectus maxime velit placeat vitae.
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="dark:bg-adoplight text-adopdark dark:shadow-md cursor-pointer" style={itemCard}>
+                        <div className="flex relative">
+                            <img className="rounded-md" src="https://chpic.su/_data/stickers/s/Sad_Kyaru_chan/Sad_Kyaru_chan_033.webp" alt="" />
+                        </div>
+                        <div className="p-2">
+                            <div className="flex relative">
+                                <img className="rounded-full h-10" src="https://chpic.su/_data/stickers/s/Sad_Kyaru_chan/Sad_Kyaru_chan_005.webp" alt="" />
+                                <div className="text-sm absolute right-0 bg-adoppix rounded-sm text-adoplight pl-1 pr-1 bottom-14 m-1">
+                                    200 Ac
+                                </div>
+                                <div className="pl-3 truncate">
+                                    <div className="text-base">
+                                        Random cat Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum perferendis tempore voluptate, sit tenetur quam quo soluta! Autem reiciendis facilis, sunt, cumque aliquam tempore provident nemo maiores neque possimus ipsam.
+                                    </div>
+                                    <div className="text-xs ml-3">
+                                        this cat is some Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta molestias cumque quae fugit a. Voluptates, explicabo. Eveniet similique error omnis laudantium consequatur ex, aspernatur consectetur, delectus maxime velit placeat vitae.
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div className="dark:bg-adoplight text-adopdark dark:shadow-md cursor-pointer" style={itemCard}>
+                        <div className="flex relative">
+                            <img className="rounded-md" src="https://chpic.su/_data/stickers/s/Sad_Kyaru_chan/Sad_Kyaru_chan_033.webp" alt="" />
+                        </div>
+                        <div className="p-2">
+                            <div className="flex relative">
+                                <img className="rounded-full h-10" src="https://chpic.su/_data/stickers/s/Sad_Kyaru_chan/Sad_Kyaru_chan_005.webp" alt="" />
+                                <div className="text-sm absolute right-0 bg-adoppix rounded-sm text-adoplight pl-1 pr-1 bottom-14 m-1">
+                                    200 Ac
+                                </div>
+                                <div className="pl-3 truncate">
+                                    <div className="text-base">
+                                        Random cat Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum perferendis tempore voluptate, sit tenetur quam quo soluta! Autem reiciendis facilis, sunt, cumque aliquam tempore provident nemo maiores neque possimus ipsam.
+                                    </div>
+                                    <div className="text-xs ml-3">
+                                        this cat is some Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta molestias cumque quae fugit a. Voluptates, explicabo. Eveniet similique error omnis laudantium consequatur ex, aspernatur consectetur, delectus maxime velit placeat vitae.
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="dark:bg-adoplight text-adopdark dark:shadow-md cursor-pointer" style={itemCard}>
+                        <div className="flex relative">
+                            <img className="rounded-md" src="https://chpic.su/_data/stickers/s/Sad_Kyaru_chan/Sad_Kyaru_chan_033.webp" alt="" />
+                        </div>
+                        <div className="p-2">
+                            <div className="flex relative">
+                                <img className="rounded-full h-10" src="https://chpic.su/_data/stickers/s/Sad_Kyaru_chan/Sad_Kyaru_chan_005.webp" alt="" />
+                                <div className="text-sm absolute right-0 bg-adoppix rounded-sm text-adoplight pl-1 pr-1 bottom-14 m-1">
+                                    200 Ac
+                                </div>
+                                <div className="pl-3 truncate">
+                                    <div className="text-base">
+                                        Random cat Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum perferendis tempore voluptate, sit tenetur quam quo soluta! Autem reiciendis facilis, sunt, cumque aliquam tempore provident nemo maiores neque possimus ipsam.
+                                    </div>
+                                    <div className="text-xs ml-3">
+                                        this cat is some Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta molestias cumque quae fugit a. Voluptates, explicabo. Eveniet similique error omnis laudantium consequatur ex, aspernatur consectetur, delectus maxime velit placeat vitae.
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default MarketItemSection;
--- src/routes/market/market.component.jsx
+++ src/routes/market/market.component.jsx
@@ -1,12 +1,15 @@
 import MarketFilter from "../../components/market/market-filter"
+import MarketItemSection from "../../components/market/market-itemSection"
 export const Market = () => {
     return(
-        <div className="grid grid-cols-12 gap-4 container px-20 dark:bg-adopdark bg-adoplight min-h-screen">
-            <div className="col-span-3">
-                <MarketFilter></MarketFilter>
-            </div>
-            <div className="col-span-9">
-
+        <div className="dark:bg-adopdark bg-adoplight min-h-screen">
+            <div className="px-20 w-full static">
+                <div className="w-1/4 m-0 static">
+                    <MarketFilter></MarketFilter>
+                </div>
+                <div className="w-3/4 inline-block">
+                    <MarketItemSection></MarketItemSection>
+                </div>
             </div>
         </div>
     )
