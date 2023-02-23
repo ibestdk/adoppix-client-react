82d5b609a8af8ebde8cd01c25f405fb29707ebd6
2son4u <87467607+2son4u@users.noreply.github.com>
2023-02-24 01:54:30 +0700

    feat : everything in half night
    -market feed refactor
    -market filter disabled
    -market item section disabled
    -market owl created
    -market owl component created
    -market index component call feed and owl created
    -market item component fix

--- src/components/market/market-index/market-itemSection.jsx
+++ src/components/market/market-index/market-itemSection.jsx
@@ -284,38 +284,49 @@ const MarketItemSection = () => {
                     ))}
                 </div>
 
-                <div className="grid grid-cols-5 gap-6 pt-2 pb-5">
+                <div className="grid grid-cols-5 gap-3 pt-2 pb-5">
 
                     {dataCard.map((data, dataIndex) => (
                         <div className="relative">
-                            <div key={dataIndex} className="dark:shadow-md shadow-[0_0_5px_lightgray] rounded-md h-72 bg-adopdark relative overflow-hidden">
+                            <div key={dataIndex} className="dark:shadow-md shadow-[0_0_5px_lightgray] rounded-md h-60 bg-adopdark relative overflow-hidden">
                                 <img className="flex-shrink-0 min-w-full min-h-full object-cover rounded-md" src={data.img} alt="" draggable="false" />
                                 <FaStar className="text-yellow-400 h-6 w-6 absolute top-2 right-2 hover:text-yellow-300 cursor-pointer"/>
-                                <div className="h-16 hover:h-36 absolute w-full bottom-0 duration-300">
-                                    <TbBusinessplan className="bg-adoppix rounded-full p-[3px] h-6 w-6 right-1 top-[-30px] absolute text-adoplight"/>
-                                    <div className="text-adoplight absolute z-50 text-xl pt-0 pl-3 truncate max-w-[150px] cursor-default">
+                                <TbBusinessplan className="bg-adoppix right-2 top-10 absolute rounded-full p-[3px] h-6 w-6 text-adoplight"/>
+                                <div className="relative">
+                                    
+                                    <div>
+                                    <div className="h-16 hover:h-36 absolute w-full bottom-0 duration-300">
+                                    
+                                    <div className="text-adoplight text-xs absolute z-50 top-1 pl-3 max-w-[150px] overflow-y-hidden text-ellipsis max-h-[30px] cursor-default">
                                         <b>
                                             {data.title}
                                         </b>
                                     </div>
-                                    <div className="flex relative z-50 top-8 cursor-pointer">
-                                        <img className="rounded-full h-6 w-6 outline outline-offset-0 outline-2 outline-adoppix absolute left-3" src={data.profileImg} alt="" draggable="false" />
-                                        <div className="absolute left-11 max-w-[80px] truncate text-adoplight">
-                                            {data.username} 
+                                    <div className="flex relative z-50 top-10 cursor-pointer">
+                                        <img className="rounded-full h-4 w-4 outline outline-offset-0 outline-2 outline-adoppix absolute left-3" src={data.profileImg} alt="" draggable="false" />
+                                        <div className="absolute left-9 max-w-[85px] text-adoplight text-xs">
+                                            <div className="relative">
+                                                <div className="max-w-[70px] w-fit truncate">
+                                                    {data.username}
+                                                </div>
+                                                <div className="h-4">
+                                                    <GoVerified className="absolute top-[2px] right-[-15px] text-green-400 cursor-default"/>
+                                                </div>
+                                            </div>
                                         </div>
-                                        <GoVerified className="absolute top-[5px] right-[96px] text-green-400 h-4 cursor-default"/>
+                                        
                                     </div>
-                                    <div className="text-3xl text-adoppix z-50 absolute right-3 cursor-default">
+                                    <div className="text-base text-adoppix z-50 absolute right-3 top-1 cursor-default">
                                         <b>
                                             {data.price}
                                         </b>
                                     </div>
-                                    <div className="text-sm text-adoplight z-50 absolute right-3 top-9 cursor-default">
+                                    <div className="text-xs text-adoplight z-50 absolute right-3 top-8 cursor-default">
                                         <b>
                                             เหลือ {data.amountLeft} ชิ้น
                                         </b>
                                     </div>
-                                    <div className="text-xs absolute z-50 w-[95px] break-words overflow-y-hidden text-ellipsis max-h-[50px] top-[60px] left-11 cursor-default text-adoplight">
+                                    <div className="text-xs absolute z-50 w-[95px] break-words overflow-y-hidden text-ellipsis max-h-[50px] top-[60px] left-8 cursor-default text-adoplight">
                                         {data.description}
                                     </div>
 
@@ -326,16 +337,19 @@ const MarketItemSection = () => {
                                         {data.tag[0]}
                                     </div>
 
-                                    <div className="text-base px-7 py-[1px] w-[80] bg-adoppix rounded-md absolute z-50 right-3 top-16 cursor-pointer hover:bg-blue-500 duration-300 hover:scale-105 text-adoplight">
+                                    <div className="text-xs px-7 py-[1px] w-[8] bg-adoppix rounded-md absolute z-50 right-2 top-16 cursor-pointer hover:bg-blue-500 duration-300 hover:scale-105 text-adoplight">
                                         <b>
                                             ซื้อ
                                         </b>
                                     </div>
-                                    <div className="text-xs px-2 py-[4px] w-[80] bg-yellow-400 rounded-md absolute z-50 right-3 top-24 cursor-pointer hover:bg-yellow-500 duration-300 hover:scale-105 text-adoplight">
+                                    <div className="text-xs px-1 py-[1px] w-[8] bg-yellow-400 rounded-md absolute z-50 right-2 top-[90px] cursor-pointer hover:bg-yellow-500 duration-300 hover:scale-105 text-adoplight">
                                         เพิ่มลงตะกร้า
                                     </div>
-                                    <div className="bg-adopsoftdark opacity-40 w-full h-full hover:brightness-50 duration-300"></div>
+                                    <div className="bg-adopsoftdark opacity-75 w-full h-full brightness-75 duration-300"></div>
                                 </div>
+                                    </div>
+                                </div>
+                                
                             </div>
 
                             {/* <div className="absolute text-base bg-green-400 rounded-md text-adoplight px-4 shadow-md py-[1px] top-[-12px] left-[-10px] z-50">
--- src/routes/market/market-index/market-index.component.jsx
+++ src/routes/market/market-index/market-index.component.jsx
@@ -1,19 +1,21 @@
 import MarketFilter from "../../../components/market/market-index/market-filter"
 import MarketItemSection from "../../../components/market/market-index/market-itemSection"
+import { MarketFeed } from "../../../components/market/market-index/market-feed"
 
 export const MarketIndex = () => {
     return(
         <div className="dark:bg-adopdark bg-adoplight min-h-screen">
             <div className="relative">
-                <div className="container m-auto">
-                <div className="grid grid-cols-12 gap-4">
-                    <div className="ml-5 mr-5 col-span-2">
+                <div className="container px-14 m-auto">
+                {/* <div className="grid grid-cols-5 gap-4">
+                    <div className="ml-5 mr-5 col-span-1">
                         <MarketFilter/>
                     </div>
-                    <div className="mr-5 col-span-10 w-full mt-[84px]">
+                    <div className="mr-5 col-span-4 w-full mt-[84px]">
                         <MarketItemSection/>
                     </div>
-                </div>
+                </div> */}
+                    <MarketFeed/>
                 </div>
             </div>
         </div>
