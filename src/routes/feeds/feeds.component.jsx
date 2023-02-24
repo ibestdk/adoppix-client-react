45dc73f3c07282724f638462f3bfb90bc101a0b0
jirat deekhum <jirat.dk@gmail.com>
2023-02-24 09:20:22 +0700

    last push

--- src/routes/feeds/feeds.component.jsx
+++ src/routes/feeds/feeds.component.jsx
@@ -1,10 +1,9 @@
- export const Feeds = () => {
-
-    return (
-
-        <div>
-        Feeds Page Rendered
-        </div>
-    )
-}
-
+export const Feeds = () => {
+  return (
+    <div className="px-[25%] pt-20 bg-adopdark">
+      <div className="container rounded-lg  mx-auto h-[2000px] bg-adopsoftdark">
+      
+      </div>
+    </div>
+  );
+};
