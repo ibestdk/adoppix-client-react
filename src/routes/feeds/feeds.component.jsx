7c60b26313010cc6d79241653e747bf3e8ddc49f
jirat deekhum <jirat.dk@gmail.com>
2023-03-06 08:05:57 +0700

    add layout for feeds

--- src/routes/feeds/feeds.component.jsx
+++ src/routes/feeds/feeds.component.jsx
@@ -1,8 +1,83 @@
+import { BsFillImageFill } from "react-icons/bs";
+
 export const Feeds = () => {
   return (
-    <div className="px-[25%] pt-20 bg-adopdark">
-      <div className="container rounded-lg  mx-auto h-[2000px] bg-adopsoftdark">
-      
+    <div>
+      <div className="px-[25%] pt-5 bg-adopdark">
+        <div className="container rounded-lg  mx-auto ">
+          <div className="flex">
+            <div className="sticky top-12 h-screen w-40 ">
+              <div>asdasdasd</div>
+              <div>asdasdasd</div>
+              <div>asdasdasd</div>
+              <div>asdasdasd</div>
+              <div>asdasdasd</div>
+            </div>
+            <main className="flex-1">
+              <div className="p-5 dark:bg-adopsoftdark m-4 rounded-lg">
+                <div>
+                  <div className="flex">
+                    <div className="relative">
+                      <img
+                        className="rounded-full w-[40px] h-[40px] "
+                        src="https://pix.adoppix.com/public/43212205-5814-4274-a04d-748a13a15a7c.jpg"
+                      />
+                    </div>
+                    <div className=" rounded-2xl bg-adopdark w-[80%] mx-4">
+              
+                    </div>
+                    <div className="m-auto "><BsFillImageFill/></div>
+                  </div>
+                </div>
+              </div>
+              <div className="p-5 m-4  rounded-lg  bg-adopsoftdark">
+                <img
+                  className="rounded-lg"
+                  src="https://pix.adoppix.com/public/43212205-5814-4274-a04d-748a13a15a7c.jpg"
+                />
+              </div>
+              <div className="p-5 m-4  rounded-lg  bg-adopsoftdark">
+                <img
+                  className="rounded-lg"
+                  src="https://pix.adoppix.com/public/43212205-5814-4274-a04d-748a13a15a7c.jpg"
+                />
+              </div>
+              <div className="p-5 m-4  rounded-lg  bg-adopsoftdark">
+                <img
+                  className="rounded-lg"
+                  src="https://pix.adoppix.com/public/43212205-5814-4274-a04d-748a13a15a7c.jpg"
+                />
+              </div>
+              <div className="p-5 m-4  rounded-lg  bg-adopsoftdark">
+                <img
+                  className="rounded-lg"
+                  src="https://pix.adoppix.com/public/43212205-5814-4274-a04d-748a13a15a7c.jpg"
+                />
+              </div>
+              <div className="p-5 m-4  rounded-lg  bg-adopsoftdark">
+                <img
+                  className="rounded-lg"
+                  src="https://pix.adoppix.com/public/43212205-5814-4274-a04d-748a13a15a7c.jpg"
+                />
+              </div>
+              <div className="p-5 m-4  rounded-lg  bg-adopsoftdark">
+                <img
+                  className="rounded-lg"
+                  src="https://pix.adoppix.com/public/43212205-5814-4274-a04d-748a13a15a7c.jpg"
+                />
+              </div>
+              <div className="p-5 m-4  rounded-lg  bg-adopsoftdark">
+                <img
+                  className="rounded-lg"
+                  src="https://pix.adoppix.com/public/43212205-5814-4274-a04d-748a13a15a7c.jpg"
+                />
+              </div>
+            </main>
+            <div className="sticky top-12 h-screen w-40 ">
+ 
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
