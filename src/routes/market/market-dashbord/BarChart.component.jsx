3e7f3fdc24675202a3f87102a88e5362cc4dd865
2son4u <87467607+2son4u@users.noreply.github.com>
2023-05-29 14:30:49 +0700

    market graph

--- src/routes/market/market-dashbord/BarChart.component.jsx
+++ src/routes/market/market-dashbord/BarChart.component.jsx
@@ -1,14 +1,34 @@
 import React from 'react';
 import { Bar } from 'react-chartjs-2';
 
-const BarChart = () => {
+const BarChart = ({datas}) => {
   // Sample data for the chart
+    const monthFinder = (month) => {
+        switch (month) {
+            case 1: return "January";
+            case 2: return "February";
+            case 3: return "March";
+            case 4: return "April";
+            case 5: return "May";
+            case 6: return "June";
+            case 7: return "July";
+            case 8: return "August";
+            case 9: return "September";
+            case 10: return "October";
+            case 11: return "November";
+            case 12: return "December";
+        }
+    }
+    console.log(datas.data.date1)
+
   const data = {
-    labels: ['January', 'February', 'March'],
+    // 
+    labels: [monthFinder(datas.data.date1), monthFinder(datas.data.date2), monthFinder(datas.data.date3)],
     datasets: [
       {
-        label: 'Sales',
-        data: [120, 150, 200],
+        label: 'ยอดขายสินค้าทั้งหมดในเว็บประจำเดือน',
+        //datas.Total1, datas.Total2, datas.Total3
+        data: [datas.data.total1, datas.data.total2, datas.data.total3],
         backgroundColor: 'rgba(54, 162, 235, 0.5)', // Bar color
       },
     ],
--- src/routes/market/market-dashbord/market-dash.component.jsx
+++ src/routes/market/market-dashbord/market-dash.component.jsx
@@ -14,37 +14,86 @@ import Chart from 'chart.js/auto';
 
 
 export const MarketDashBord = () => {
+    useEffect(() => {
+        fetchData();
+        allDash();
+    }, []);
 
     const [user, setUser] = useState();
     const userSet = (e) => {
         setUser(e.target.value);
     }
 
+    const [datas, setDatas] = useState();
+    const fetchData = async () => {
+        try {
+            const response = await axios.get('https://localhost:7226/api/Dashbord');
+            setDatas(response.data);
+            console.log(response.data);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
+
+    const [all, setall] = useState();
+    const allDash = async () => {
+        try {
+            const response = await axios.get('https://localhost:7226/api/Dashbord/All');
+            setall(response.data);
+            console.log(response.data);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
+
+    const [dataNs, setDataNs] = useState();
+    const dashByName = async (name) => {
+        try {
+            const response = await axios.get(`https://localhost:7226/api/Dashbord/${name}`);
+            setDataNs(response.data);
+            console.log(data);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
+
     return (
         <div>
-            <div className="mt-40 mx-auto w-full h-full min-h-screen">
-                <div className="mx-auto pl-80">
+            {datas && all && (
+                <div className="mt-40 mx-auto w-full h-full min-h-screen">
+                    <div className="mx-auto pl-80">
                     <div>
-                        <div className="text-white">
-                            สินค้าที่ขายออกทั้งหมด 3 เดือน
-                        </div>
-                        <div className="max-h-[600px]">
-                            <BarChart />
-                        </div>
-                    </div>
-                    <div className="my-20">
-                        <div className="text-white">
-                            สินค้าที่ขายออกทั้งหมด 3 เดือน ของผู้ใช้
-                            <div className="ml-5 inline-block rounded-md">
-                                <input onChange={userSet} className="text-adopdark rounded-md outline outline-adoplighticon focus:outline-adoplighticon focus:scale-105 duration-300 focus:border-adoplighticon" type="text" />
+                            <div className="text-adoppix text-3xl my-10 pl-[405px]">
+                                สินค้าที่ขายออกทั้งหมดในเว็บ {all && all.data} ชิ้น
                             </div>
                         </div>
-                        <div className="max-h-[600px]">
-                            <BarChart />
+                        <div>
+                            <div className="text-white">
+                                สินค้าที่ขายออกทั้งหมด 3 เดือน
+                            </div>
+                            <div className="max-h-[600px]">
+                                <BarChart datas={datas} />
+                            </div>
+                        </div>
+                        <div className="my-20">
+                            <div className="text-white">
+                                สินค้าที่ขายออกทั้งหมด 3 เดือน ของผู้ใช้
+                                <div className="ml-5 inline-block rounded-md">
+                                    <input onChange={userSet} className="text-adopdark rounded-md outline outline-adoplighticon focus:outline-adoplighticon focus:scale-105 duration-300 focus:border-adoplighticon" type="text" />
+                                </div>
+                                <p onClick={() => dashByName(user)} className="inline-block ml-5 bg-adoppix text-adoplight rounded-md py-2 px-4 cursor-pointer hover:bg-blue-500 hover:scale-105 duration-300">
+                                    ดูกราฟ
+                                </p>    
+                            </div>
+                            {dataNs && (
+                                <div className="max-h-[600px]">
+                                    <BarChart datas={dataNs} />
+                                </div>
+                            )}
                         </div>
                     </div>
                 </div>
-            </div>
+            )}
         </div>
     )
 }
